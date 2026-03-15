8f606130b38c06f583f708a211e61479f88cdaa1
rerxyz_ <reynard16rande@gmail.com>
2026-03-15 15:19:49 +0700

    second commit

--- TP_MODUL4_103022400125/Program.cs
+++ TP_MODUL4_103022400125/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+public class KodePos
+{
+    public (string[] kelurahan, string[] kodePos) getKodePos()
+    {
+        string[] kelurahan = { "Batununggal", "Kujangsari", "Mengger", "Wates", "Cijaura", "Jatisari", 
+            "Margasari", "Sekejati", "Kebonwaru", "Maleer" };
+
+        string[] kodepos = { "40266", "40287", "40267", "40256", "40287", "40286", "40286", "40286", "40272", "40274" };
+
+        return (kelurahan, kodepos);
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        KodePos kode = new KodePos();
+
+        var (kelurahan, kodepos) = kode.getKodePos();
+
+        Console.WriteLine("=== Daftar Kode Pos ===");
+
+        for (int i = 0; i < kelurahan.Length; i++)
+        {
+            Console.WriteLine("Kelurahan " + kelurahan[i] + " : " + kodepos[i]);
+        }
+    }
+}
