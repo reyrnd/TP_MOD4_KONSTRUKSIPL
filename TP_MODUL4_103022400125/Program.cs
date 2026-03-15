b2d6551ccf52737c93498b1466f89437e1ade1a0
rerxyz_ <reynard16rande@gmail.com>
2026-03-15 15:25:19 +0700

    menambahkan class DoorMachine

--- TP_MODUL4_103022400125/Program.cs
+++ TP_MODUL4_103022400125/Program.cs
@@ -6,9 +6,52 @@ public class KodePos
         string[] kelurahan = { "Batununggal", "Kujangsari", "Mengger", "Wates", "Cijaura", "Jatisari", 
             "Margasari", "Sekejati", "Kebonwaru", "Maleer" };
 
-        string[] kodepos = { "40266", "40287", "40267", "40256", "40287", "40286", "40286", "40286", "40272", "40274" };
+        string[] kodepos = { "40266", "40287", "40267", "40256", "40287", "40286", "40286", "40286", 
+            "40272", "40274" };
 
         return (kelurahan, kodepos);
+    }
+}
+
+public class DoorMachine
+{
+    enum State
+    {
+        Terkunci,
+        Terbuka
+    }
+
+    private State stateSaatIni = State.Terkunci;
+
+    public DoorMachine()
+    {
+        Console.WriteLine("Pintu terkunci"); 
+    }
+
+    public void BukaPintu()
+    {
+        if (stateSaatIni == State.Terkunci)
+        {
+            stateSaatIni = State.Terbuka;
+            Console.WriteLine("Pintu tidak terkunci");
+        }
+        else
+        {
+            Console.WriteLine("Pintu sudah terbuka, tidak bisa buka lagi");
+        }
+    }
+
+    public void KunciPintu()
+    {
+        if (stateSaatIni == State.Terbuka)
+        {
+            stateSaatIni = State.Terkunci;
+            Console.WriteLine("Pintu terkunci");
+        }
+        else
+        {
+            Console.WriteLine("Pintu sudah terkunci, tidak bisa kunci lagi");
+        }
     }
 }
 
@@ -16,6 +59,7 @@ public class Program
 {
     public static void Main()
     {
+
         KodePos kode = new KodePos();
 
         var (kelurahan, kodepos) = kode.getKodePos();
@@ -26,5 +70,22 @@ public class Program
         {
             Console.WriteLine("Kelurahan " + kelurahan[i] + " : " + kodepos[i]);
         }
+
+        Console.WriteLine("Simulasi DoorMachine");
+        Console.WriteLine();
+
+        DoorMachine pintu = new DoorMachine(); 
+
+        Console.WriteLine("Aksi: BukaPintu");
+        pintu.BukaPintu(); 
+
+        Console.WriteLine("Aksi: KunciPintu");
+        pintu.KunciPintu(); 
+
+        Console.WriteLine("Aksi: BukaPintu lagi");
+        pintu.BukaPintu(); 
+
+        Console.WriteLine("Aksi: BukaPintu lagi (state sudah Terbuka)");
+        pintu.BukaPintu();
     }
 }
